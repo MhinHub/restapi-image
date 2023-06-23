8de77a0c5aed6d8b702ef2623f11679584c09681
MinHub <iskandarm20d@student.unhas.ac.id>
2023-06-23 22:55:37 +0800

    :hammer: Update configuration

--- Backend/api/index.js
+++ Backend/api/index.js
@@ -1,3 +1,16 @@
-import app from "../app";
+import express from "express";
+import FileUpload from "express-fileupload";
+import cors from "cors";
+import ProductRoute from "../routes/ProductRoute.js";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(FileUpload());
+app.use(express.static("public"));
+app.use(ProductRoute);
+
+app.listen(process.env.PORT, () => console.log('Server Up and Running...'));
 
 export default app;
